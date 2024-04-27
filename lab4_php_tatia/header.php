2f47762d343ca743a582ba62deadbf418b6e4fb3
tatiadeswitaa <129940568+tatiadeswitaa@users.noreply.github.com>
2024-04-27 18:50:30 +0700

    Add files via upload

---
+++ lab4_php_tatia/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contoh Modularisasi</title>
+    <link href="style.css" rel="stylesheet" type="text/stylesheet"
+media="screen" />
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Modularisasi Menggunakan Require</h1>
+        </header>
+        <nav>
+            <a href="home.php">Home</a>
+            <a href="about.php">Tentang</a>
+            <a href="kontak.php">Kontak</a>
+</nav>
+</body>
+<html>
+
+<h2> menambahkan footer <h2>
+    <footer>
+        <p>&copy; 2021, Informatika, Universitas Pelita Bangsa</p>
+    </footer>
+</div>
+</body>
+</html>
+
+<h2>home php<h2>
+<?php require('header.php');?>
+
+<div class="content">
+    <h2>Ini Halaman Home</h2>
+    <p>Ini adalah bagian content dari halaman.</p>
+</div> 
+</body>
+<html>
